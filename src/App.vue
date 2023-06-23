a7433e32ef1da2631bff979566a3e2c89fe451aa
Konyuka <michaelsaiba84@gmail.com>
2023-06-24 00:41:29 +0300

    Login Page Design

--- src/App.vue
+++ src/App.vue
@@ -4,21 +4,11 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <!-- <RouterLink to="/">Home</RouterLink> -->
+  <div class="w-[360px] h-[800px] m-auto ">
+    <RouterView />
+  </div>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
-  
 </template>
 
 <style scoped>
